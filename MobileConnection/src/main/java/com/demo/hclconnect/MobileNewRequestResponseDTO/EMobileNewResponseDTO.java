95e22a640666238f57db57226ff1c34f0e7fea75
Jha <JhaA@haufe.com>
2020-11-07 13:13:15 +0530

    added lombok dependency

---
+++ MobileConnection/src/main/java/com/demo/hclconnect/MobileNewRequestResponseDTO/EMobileNewResponseDTO.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.demo.hclconnect.MobileNewRequestResponseDTO;
+
+/**
+ * @author JhaA
+ *
+ */
+public class EMobileNewResponseDTO {
+
+	private String newRequestResponseDTO;
+
+	public String getNewRequestResponseDTO() {
+		return newRequestResponseDTO;
+	}
+
+	public void setNewRequestResponseDTO(String newRequestResponseDTO) {
+		this.newRequestResponseDTO = newRequestResponseDTO;
+	}
+			
+}
+
+
+
