8c7af125c096cd035efcd3ff2870bdbbd04b0723
Jha <JhaA@haufe.com>
2020-11-07 12:53:28 +0530

    added controller and Dto class for new connection request

---
+++ MobileConnection/src/main/java/com/demo/hclconnect/MobileNewRequestDTO/EMobileNewRequestDTO.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package com.demo.hclconnect.MobileNewRequestDTO;
+
+/**
+ * @author JhaA
+ *
+ */
+public class EMobileNewRequestDTO {
+
+	private String irstName;
+	private String lastName;
+	private int 	refMobileNumber;
+	private String emailId ;
+	private String govtId;
+	private String planCode;
+	private String status;
+	private String RequestId;
+	private String documentFile;
+	public String getIrstName() {
+		return irstName;
+	}
+	public void setIrstName(String irstName) {
+		this.irstName = irstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getRefMobileNumber() {
+		return refMobileNumber;
+	}
+	public void setRefMobileNumber(int refMobileNumber) {
+		this.refMobileNumber = refMobileNumber;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getGovtId() {
+		return govtId;
+	}
+	public void setGovtId(String govtId) {
+		this.govtId = govtId;
+	}
+	public String getPlanCode() {
+		return planCode;
+	}
+	public void setPlanCode(String planCode) {
+		this.planCode = planCode;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getRequestId() {
+		return RequestId;
+	}
+	public void setRequestId(String requestId) {
+		RequestId = requestId;
+	}
+	public String getDocumentFile() {
+		return documentFile;
+	}
+	public void setDocumentFile(String documentFile) {
+		this.documentFile = documentFile;
+	}
+	
+	
+}
+
+
+
